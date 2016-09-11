9cf08ecec1b9c0cfd0bd91fdd0db7d9c2750aa01
heller <heller@H-L-PC>
2016-09-11 17:42:17 +0800

    test lambda expression

--- javase8/src/main/java/ch01/exercise/Ch01.java
+++ javase8/src/main/java/ch01/exercise/Ch01.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.BiFunction;
 
 public class Ch01 {
 
@@ -28,5 +30,20 @@ public class Ch01 {
         System.out.println(Arrays.toString(pdfFiles));
     }
 
+    @Test
+    public void testFunction() {
+        BiFunction<String, String, Integer> comparator =
+                (first, second) -> Integer.compare(first.length(), second.length());
+        BiFunction<String, String, Integer> comparator2 = String::compareTo;
+
+        Comparator<String> stringComparator = (String x, String y) -> x.compareTo(y);
+        Comparator<String> stringComparator2 = String::compareTo;// 更简便的写法(method reference: 方法引用)
+
+        // Lambda表达式可以很方便的使用，被自由的转化为所需要的函数式表达式
+        //（String::compareTo这个Lambda表达式轻松的赋值给了不同的函数式接口）
+        // 事实上，函数式接口转换是我们在Java中使用lambda表达式能做的唯一一件事情
+        System.out.println(comparator2);
+        System.out.println(stringComparator2);
+    }
 
 }
