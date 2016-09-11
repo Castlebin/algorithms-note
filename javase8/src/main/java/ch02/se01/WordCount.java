527ef320cd29872809af137f971158926fafedd1
Yuxin Gao <Yuxin Gao>
2016-09-11 22:52:11 +0800

    ch02 se01 long word count

---
+++ javase8/src/main/java/ch02/se01/WordCount.java
@@ -0,0 +1,35 @@
+package ch02.se01;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class WordCount {
+    private Logger log = Logger.getGlobal();
+
+    private static String filePath = "src/main/resources/war-and-peace.txt";
+    private String content = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+    private List<String> words = Arrays.asList(content.split("[\\P{L}]+"));
+
+    public WordCount() throws IOException {
+    }
+
+    @Test
+    public void testStream() throws IOException {
+        long count = words.stream().filter(w -> w.length() > 12).count();
+        log.info("count: " + count);
+    }
+
+    @Test
+    public void testParallelStream() throws IOException {
+        long count = words.parallelStream().filter(w -> w.length() > 12).count();
+        log.info("count: " + count);
+    }
+
+}
