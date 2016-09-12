cf197cd0ff5cb0826d5277340b9dba5ddde5f3ca
heller <heller@H-L-PC>
2016-09-12 14:53:32 +0800

    2. Add Two Numbers 取巧

--- leetcode/src/main/java/leetcode/N002.java
+++ leetcode/src/main/java/leetcode/N002.java
@@ -1,14 +1,52 @@
 package leetcode;
 
+import java.math.BigDecimal;
+
 public class N002 {
+
+    public BigDecimal getNumOfListNode(ListNode listNode) {
+        return new BigDecimal(getNumStrOfListNode(listNode));
+    }
+
+    public String getNumStrOfListNode(ListNode listNode) {
+        return new StringBuilder(getNumStrOfListNodeReverse(listNode)).reverse().toString();
+    }
+
+    public String getNumStrOfListNodeReverse(ListNode listNode) {
+        String numStr = "" + listNode.val;
+        if (listNode.next == null) {
+            return numStr;
+        } else {
+            return numStr + getNumStrOfListNodeReverse(listNode.next);
+        }
+    }
+
+    public ListNode createListNodeFromNum(BigDecimal num) {
+        String numReverseStr = new StringBuilder(num.toPlainString()).reverse().toString();
+
+        char[] chars = numReverseStr.toCharArray();
+        ListNode result = new ListNode(new Integer(chars[0]+""));
+        ListNode n = result;
+        for (int i=1; i<chars.length; i++) {
+            ListNode newNode = new ListNode(new Integer(chars[i]+""));
+            n.next = newNode;
+            n = newNode;
+        }
+
+        return result;
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-
-        return null;
+        return createListNodeFromNum(getNumOfListNode(l1).add(getNumOfListNode(l2)));
     }
 
     public static void main(String[] args) {
+        N002 n002 = new N002();
+        ListNode l1 = n002.createListNodeFromNum(new BigDecimal("999999999999999"));
+        ListNode l2 = n002.createListNodeFromNum(new BigDecimal("99999999999999999999999991"));
 
+        ListNode result = n002.addTwoNumbers(l1, l2);
+        System.out.println(n002.getNumOfListNode(result));
     }
 
     class ListNode {
