77061e1a7f44bb93f862d60de358503e0ee432a4
heller <heller@H-L-PC>
2016-09-12 13:42:53 +0800

    N002

---
+++ leetcode/src/main/java/leetcode/N002.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class N002 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
