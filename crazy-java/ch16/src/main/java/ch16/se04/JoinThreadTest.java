c19cae276381d1fd3006ab4807e119265e28c5f9
heller <heller@H-PC>
2016-10-16 14:28:01 +0800

    16.4.1 join线程

--- crazy-java/ch16/src/main/java/ch16/se04/JoinThreadTest.java
+++ crazy-java/ch16/src/main/java/ch16/se04/JoinThreadTest.java
@@ -12,6 +12,17 @@ public class JoinThreadTest {
             }
             System.out.println(Thread.currentThread().getName() + " " + i);
         }
+
+        // main 线程必须等待5个join进来的线程执行完毕才能继续执行
+        for (int i = 0; i < 10; i++) {
+            if (i < 5) {
+                TestThread testThread = new TestThread("join的线程 - " + i);
+                testThread.start();
+                testThread.join();
+            }
+
+            System.out.println(Thread.currentThread() + " " + i);
+        }
     }
 }
 
