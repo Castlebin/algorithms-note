60078aa5d417f9af4394f661746c299dc1d771cd
Castlebin <Castlebin@gmail.com>
2016-10-17 21:06:40 +0800

    Files Test

--- crazy-java/ch15/src/main/java/ch15/se10/FilesTest.java
+++ crazy-java/ch15/src/main/java/ch15/se10/FilesTest.java
@@ -3,6 +3,7 @@ package ch15.se10;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileStore;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -21,9 +22,27 @@ public class FilesTest {
         // 获取指定文件的大小
         System.out.println("文件大小：" + Files.size(Paths.get(".gitignore")));
 
-        // 直接将多行内容写入文件
         List<String> poem = new ArrayList<String>() {{
-            add("aaaaa");
+            add("我爱你");
+            add("但我不敢说出口");
+            add("我怕我说了");
+            add("就会死");
+            add("我不怕死");
+            add("我怕我死了");
+            add("再也没有人像我这样爱你");
         }};
+        // 直接将多行内容写入文件
+        Files.write(Paths.get("poem.txt"), poem, StandardCharsets.UTF_8);
+
+        // 使用Java 8 新增的Stream API列出当前目录下的所有文件和子目录
+        Files.list(Paths.get(".")).forEach(System.out::println);
+
+        // 读取文件内容
+        Files.lines(Paths.get("poem.txt"), StandardCharsets.UTF_8).forEach(System.out::println);
+
+        // FileStore类
+        FileStore rootStore = Files.getFileStore(Paths.get("/"));
+        System.out.println("总空间：" + rootStore.getTotalSpace());
+        System.out.println("可用空间：" + rootStore.getUsableSpace());
     }
 }
