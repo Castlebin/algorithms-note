154428b969d886a1b2a934c291020f335c36f4d0
Castlebin <Castlebin@gmail.com>
2016-10-22 15:28:07 +0800

    普通单向链表的深拷贝

---
+++ java-test/src/main/java/linkedlist/LinkedListTest.java
@@ -0,0 +1,60 @@
+package linkedlist;
+
+import lombok.Data;
+
+// 定义好链表节点数据结构
+@Data
+class Node {
+    int data;
+    Node next;
+
+    public void printNode() {
+        System.out.println(data);
+        if (next != null) {
+            next.printNode();
+        }
+    }
+
+    public Node deepCopy() {
+        Node head = new Node();
+        head.data = data;
+        return deepCopy(head, next);
+    }
+
+    public Node deepCopy(Node last, Node nowToCopy) {
+        if (nowToCopy != null) {
+            Node node = new Node();
+            node.data = nowToCopy.data;
+            last.next = node;
+            deepCopy(node, nowToCopy.next);
+        }
+
+        return last;
+    }
+}
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        Node linkedList1 = buildTestListedList(5);
+        linkedList1.printNode();
+        Node linkedList2 = linkedList1.deepCopy();
+        linkedList2.printNode();
+    }
+
+    public static Node buildTestListedList(int length) {
+        if (length <= 0) {
+            return null;
+        }
+        Node head = new Node();
+        head.setData(0);
+        Node lastNode = head;
+        for (int i = 1; i < length; i++) {
+            Node node = new Node();
+            node.setData(i);
+            lastNode.setNext(node);
+            lastNode = node;
+        }
+
+        return head;
+    }
+}
