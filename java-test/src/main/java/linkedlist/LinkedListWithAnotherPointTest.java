06c49b4c8b359a9c8e418159e907ea58aa3d7ced
Castlebin <Castlebin@gmail.com>
2016-10-22 15:40:10 +0800

    带一个随机指针的链表，简化的数据结构

---
+++ java-test/src/main/java/linkedlist/LinkedListWithAnotherPointTest.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+import java.util.List;
+import java.util.Map;
+
+public class LinkedListWithAnotherPointTest {
+
+}
+
+class NodeWithPoint {
+    int data;
+    Node point;
+}
+
+class LinkedListWithAnotherPoint {
+    List<NodeWithPoint> dataList;
+    Map<Integer, Integer> indexPointMap;
+}
