78eba5b8bd0fd43970310d8812a5cbe0d0a1264c
heller <heller@H-PC>
2016-10-24 12:21:10 +0800

    threadWithTimeout

--- java-test/src/main/java/thread/ThreadTimeOutTest.java
+++ java-test/src/main/java/thread/ThreadTimeOutTest.java
@@ -46,23 +46,3 @@ public class ThreadTimeOutTest {
     }
 }
 
-class ThreadWithTimeOut<T> {
-    private Callable<T> callable;
-    private int timeout;
-    public ThreadWithTimeOut(Callable<T> callable, int timeout) {
-        this.callable = callable;
-        this.timeout = timeout;
-    }
-
-    public T runTask() throws InterruptedException, ExecutionException, TimeoutException {
-        FutureTask<T> task = new FutureTask<>(callable);
-        Thread thread = new Thread(task);
-        thread.start();
-        try {
-            return task.get(timeout, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            task.cancel(true);
-            throw e;
-        }
-    }
-}
