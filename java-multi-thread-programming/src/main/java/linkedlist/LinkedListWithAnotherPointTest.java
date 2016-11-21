85ec3e8bca9acaf061886f2bc02fb4b341a00f0f
heller <heller@H-PC>
2016-11-21 16:30:52 +0800

    Java多线程编程核心技术

---
+++ java-multi-thread-programming/src/main/java/linkedlist/LinkedListWithAnotherPointTest.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+import java.util.List;
+import java.util.Map;
+
+public class LinkedListWithAnotherPointTest {
+
+}
+
+class NodeWithPoint {
+    int data;
+    NodeWithPoint point;
+}
+
+class LinkedListWithAnotherPoint {
+    List<NodeWithPoint> dataList;
+    Map<Integer, Integer> indexPointMap;
+}
