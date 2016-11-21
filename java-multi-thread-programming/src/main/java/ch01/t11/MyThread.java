922c05019138a69bf1be2265ff3e8c014c8573a6
heller <heller@H-PC>
2016-11-21 16:35:28 +0800

    thread.interrupt()并不能终止线程

---
+++ java-multi-thread-programming/src/main/java/ch01/t11/MyThread.java
@@ -0,0 +1,16 @@
+package ch01.t11;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println("i = " + (i + 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread myThread = new MyThread();
+        myThread.start();
+        myThread.interrupt();// interrupt()并不能终止线程
+    }
+}
