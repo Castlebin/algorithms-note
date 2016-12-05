eaedf044471ca5ee99d844603ce8f56d82e45791
castlebin <castlebin@gmail.com>
2016-12-06 01:05:47 +0800

    基于TCP的RP实现

--- distribute_system/ch01/se01_tcp_rpc/service_api_impl/src/main/java/ch01/se01/se01_tcp_soa/service/impl/SayHelloServiceImpl.java
+++ distribute_system/ch01/se01_tcp_rpc/service_api_impl/src/main/java/ch01/se01/se01_tcp_soa/service/impl/SayHelloServiceImpl.java
@@ -5,6 +5,7 @@ import com.sun.deploy.util.StringUtils;
 
 public class SayHelloServiceImpl implements SayHelloService {
 
+    @Override
     public String sayHello(String name) {
         if (name == null || StringUtils.trimWhitespace(name).length() == 0) {
             name = "World";
