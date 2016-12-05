968d259a4720a398cc35b956515a1be71094dbfb
castlebin <castlebin@gmail.com>
2016-12-05 23:54:49 +0800

    Java 自带的序列化反序列机制, Hessian序列化反序列化

--- java-test/src/main/java/serialize/Person.java
+++ java-test/src/main/java/serialize/Person.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 
 import java.io.Serializable;
 
+// java 本身的序列化反序列化机制还有 hessian都要求要序列化的对象的类实现Serializable接口
 @Getter
 @Setter
 @ToString
--- java-test/src/main/java/serialize/SerializeTest.java
+++ java-test/src/main/java/serialize/SerializeTest.java
@@ -1,23 +1,50 @@
 package serialize;
 
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+
 import java.io.*;
 
 public class SerializeTest {
     public static void main(String[] args) {
         Person xiaoming = new Person("xiaoming", 18);
 
+        // java 本身的序列化反序列化机制还有 hessian都要求要序列化的对象的类实现Serializable接口
+        javaSerialize(xiaoming);
+
+        hessianSerialize(xiaoming);
+    }
+
+    private static <T extends Serializable> void javaSerialize(T serializable) {
         try (ByteArrayOutputStream os = new ByteArrayOutputStream();
              ObjectOutputStream out = new ObjectOutputStream(os)) {
-            out.writeObject(xiaoming);
+            out.writeObject(serializable);
             byte[] xiaomingBytes = os.toByteArray();
 
             // 执行反序列化
             try (ByteArrayInputStream is = new ByteArrayInputStream(xiaomingBytes);
                  ObjectInputStream in = new ObjectInputStream(is)) {
-                Person p = (Person) in.readObject();
-                System.out.println(p);
+                T readObject = (T) in.readObject();
+                System.out.println(readObject);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static <T extends Serializable> void hessianSerialize(T serializable) {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            HessianOutput out = new HessianOutput(os);
+            out.writeObject(serializable);
+            byte[] xiaomingBytes = os.toByteArray();
+
+            // 执行反序列化
+            try (ByteArrayInputStream is = new ByteArrayInputStream(xiaomingBytes)) {
+                HessianInput in = new HessianInput(is);
+                T readObject = (T) in.readObject();
+                System.out.println(readObject);
             }
         } catch (IOException e) {
             e.printStackTrace();
