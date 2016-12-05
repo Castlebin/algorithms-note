9f7813e56b148d532d911e5aa25567f3e145b501
castlebin <castlebin@gmail.com>
2016-12-06 00:26:28 +0800

    tcp soa 实现的 service impl 层

---
+++ distribute_system/ch01/se01_tcp_rpc/service_api_impl/src/main/java/ch01/se01/se01_tcp_soa/service/impl/SayHelloServiceImpl.java
@@ -0,0 +1,15 @@
+package ch01.se01.se01_tcp_soa.service.impl;
+
+import ch01.se01.se01_tcp_soa.service.SayHelloService;
+import com.sun.deploy.util.StringUtils;
+
+public class SayHelloServiceImpl implements SayHelloService {
+
+    public String sayHello(String name) {
+        if (name == null || StringUtils.trimWhitespace(name).length() == 0) {
+            name = "World";
+        }
+        return "Hello, " + name;
+    }
+
+}
