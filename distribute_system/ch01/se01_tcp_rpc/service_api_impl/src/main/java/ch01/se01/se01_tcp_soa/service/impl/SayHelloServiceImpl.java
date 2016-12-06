e860b8b7e06a2af164a0d1bf500a7c3ccab1565d
heller <heller@H-PC>
2016-12-06 10:48:01 +0800

    基于TCP的RPC实现

--- distribute_system/ch01/se01_tcp_rpc/service_api_impl/src/main/java/ch01/se01/se01_tcp_soa/service/impl/SayHelloServiceImpl.java
+++ distribute_system/ch01/se01_tcp_rpc/service_api_impl/src/main/java/ch01/se01/se01_tcp_soa/service/impl/SayHelloServiceImpl.java
@@ -1,13 +1,12 @@
 package ch01.se01.se01_tcp_soa.service.impl;
 
 import ch01.se01.se01_tcp_soa.service.SayHelloService;
-import com.sun.deploy.util.StringUtils;
 
 public class SayHelloServiceImpl implements SayHelloService {
 
     @Override
     public String sayHello(String name) {
-        if (name == null || StringUtils.trimWhitespace(name).length() == 0) {
+        if (name == null || name.trim().length() == 0) {
             name = "World";
         }
         return "Hello, " + name;
