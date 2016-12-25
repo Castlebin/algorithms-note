0ad90834d651850e2d76f5ccb8dc52f050099be2
Castlebin <Castlebin@gmail.com>
2016-12-26 00:05:05 +0800

    rabbitmq tutorial 06: rpc

--- rabbitmq-test/src/main/java/rabbitmq/tutorial/t06/RpcClient.java
+++ rabbitmq-test/src/main/java/rabbitmq/tutorial/t06/RpcClient.java
@@ -1,4 +1,67 @@
 package rabbitmq.tutorial.t06;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
 public class RpcClient {
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
+    private Connection connection;
+    private Channel channel;
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+
+    private static final ConnectionFactory connectionFactory;
+
+    static {
+        connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+    }
+
+    public RpcClient() throws IOException, TimeoutException {
+        connection = connectionFactory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    public String call(String message) throws InterruptedException {
+        String corrId = UUID.randomUUID().toString();
+        BasicProperties props = new AMQP.BasicProperties()
+                .builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+        String response;
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody());
+                break;
+            }
+        }
+
+        return response;
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        RpcClient fibonacciRpc = new RpcClient();
+
+        System.out.println(" [x] Requesting fib(30)");
+        String response = fibonacciRpc.call("30");
+        System.out.println(" [.] Got '" + response + "'");
+
+        fibonacciRpc.close();
+    }
+
 }
