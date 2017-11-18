7ffdcc4e87be621738100cd96a3b2d294ee28cbc
fengye <gaoyuxin.gyx@alibaba-inc.com>
2017-11-18 19:34:40 +0800

    basic

---
+++ leetcode/src/main/java/leetcode/N015.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class N015 {
+
+    private int[] nums = new int[] {-1, 0, 1, 2, -1, -4};
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // TODO
+
+        return result;
+    }
+
+}
