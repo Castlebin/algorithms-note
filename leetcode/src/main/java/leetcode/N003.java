cb5c1bf5693935e54f73c35121069a0a030ca27d
fengye <gaoyuxin.gyx@alibaba-inc.com>
2017-12-09 21:00:16 +0800

    3. Longest Substring Without Repeating Characters

--- leetcode/src/main/java/leetcode/N003.java
+++ leetcode/src/main/java/leetcode/N003.java
@@ -1,20 +1,20 @@
 package leetcode;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 3. Longest Substring Without Repeating Characters
-
- Given a string, find the length of the longest substring without repeating characters.
-
- Examples:
-
- Given "abcabcbb", the answer is "abc", which the length is 3.
-
- Given "bbbbb", the answer is "b", with the length of 1.
-
- Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ *
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ *
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ *
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class N003 {
     public static void main(String[] args) {
@@ -37,7 +37,7 @@ public class N003 {
         Map<Character, Integer> addCharMap = new HashMap<>();
         char[] chars = s.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if ( !addCharMap.containsKey(chars[i]) ) {
+            if (!addCharMap.containsKey(chars[i])) {
                 addCharMap.put(chars[i], i);
                 if (addCharMap.size() > result) {
                     result = addCharMap.size();
@@ -49,6 +49,20 @@ public class N003 {
             }
         }
         return result;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        if (s.length() == 0) { return 0; }
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
     }
 
 }
