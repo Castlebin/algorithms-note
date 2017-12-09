9a6623a07d904e3115ce85f7f04b051a29e5bab4
fengye <gaoyuxin.gyx@alibaba-inc.com>
2017-12-09 22:49:20 +0800

    6. ZigZag Conversion

--- leetcode/src/main/java/leetcode/N004.java
+++ leetcode/src/main/java/leetcode/N004.java
@@ -1,7 +1,22 @@
 package leetcode;
 
-import java.util.Arrays;
+/**
+    4. Median of Two Sorted Arrays
+ There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
+ Find the median of the two sorted arrays. The overall run time complexity should be    O(log (m+n)).
+
+ Example 1:
+ nums1 = [1, 3]
+ nums2 = [2]
+
+ The median is 2.0
+ Example 2:
+ nums1 = [1, 2]
+ nums2 = [3, 4]
+
+ The median is (2 + 3)/2 = 2.5
+ */
 public class N004 {
     public static void main(String[] args) {
         N004 n004 = new N004();
@@ -11,33 +26,6 @@ public class N004 {
     }
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] all = new int[nums1.length + nums2.length];
-        int i = 0, j = 0, k = 0;
-        for (; i < nums1.length && j < nums2.length; k++) {
-            if (nums1[i] < nums2[j]) {
-                all[k] = nums1[i++];
-            } else {
-                all[k] = nums2[j++];
-            }
-        }
-        if (i == nums1.length) {//nums1 全部添加完毕
-            for (;j<nums2.length;k++) {
-                all[k] = nums2[j++];
-            }
-        } else if (j == nums2.length) {// nums2 全部添加完毕（其实只能是这两者之一）
-            for (;i<nums1.length;k++) {
-                all[k] = nums1[i++];
-            }
-        }
-
-        System.out.println("nums1: " + Arrays.toString(nums1));
-        System.out.println("nums2: " + Arrays.toString(nums2));
-        System.out.println("all: " + Arrays.toString(all));
-
-        if (all.length % 2 == 1) {// 奇数个元素，取中间
-            return all[(all.length - 1) / 2];
-        } else {
-            return (all[all.length / 2] + all[all.length / 2 - 1]) / 2.0;
-        }
+        return 0;
     }
 }
