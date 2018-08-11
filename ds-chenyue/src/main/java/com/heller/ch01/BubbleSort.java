689c5ed99590b7c070bdb6b71736fd55bc48ec11
castlebin <castlebin@gmail.com>
2018-08-12 00:08:01 +0800

    优化的冒泡排序

--- ds-chenyue/src/main/java/com/heller/ch01/BubbleSort.java
+++ ds-chenyue/src/main/java/com/heller/ch01/BubbleSort.java
@@ -10,16 +10,23 @@ import static com.heller.ch01.ArrayUtil.generateArray;
 public class BubbleSort {
 
     /**
-     * 冒泡排序：
+     * 冒泡排序：（稳定的排序算法）
      * n轮循环，每轮循环中，比较两个相邻元素，如果a[i] > a[i+1]，则交换两个元素
      * 每一轮排序之后，从第n-i到n-1个元素都是有序的（大的那部分元素）
+     *
+     * 优化：当某一轮循环时，发现没有元素交换，那么说明整个序列都是排序好的了，不需要继续循环了
      */
     public static void bubbleSort(int[] array) {
         for (int j = 0; j < array.length; j++) {
+            boolean swapped = false;
             for (int i = 0; i < array.length - 1 - j; i++) {
                 if (array[i] > array[i + 1]) {
                     swap(array, i, i + 1);
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
     }
