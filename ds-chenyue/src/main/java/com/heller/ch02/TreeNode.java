ff986053598f6439430d41b7e9254432b6c614d9
castlebin <castlebin@gmail.com>
2018-08-21 23:35:05 +0800

    三、树（上）

---
+++ ds-chenyue/src/main/java/com/heller/ch02/TreeNode.java
@@ -0,0 +1,19 @@
+package com.heller.ch02;
+
+/**
+ * 二叉树 的 节点定义
+ *
+ * 方便起见，把属性都定义为public了
+ */
+public class TreeNode {
+
+    /**
+     * 表示一下节点的data域
+     */
+    public Integer data;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+}
