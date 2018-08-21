f0a30e713d2c1cb5ef6365a04dd8f306a3eba4e0
castlebin <castlebin@gmail.com>
2018-08-22 00:11:30 +0800

    树的四种遍历方式（前序、中序、后序、层次遍历）

--- ds-chenyue/src/main/java/com/heller/ch03/TreeNodeTraversal.java
+++ ds-chenyue/src/main/java/com/heller/ch03/TreeNodeTraversal.java
@@ -1,5 +1,8 @@
 package com.heller.ch03;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 二叉树的遍历
  * 1. 前序遍历（根、左子树、右子树）               （递归调用）
@@ -9,6 +12,9 @@ package com.heller.ch03;
  */
 public class TreeNodeTraversal {
 
+    /**
+     * 前序遍历（根、左子树、右子树）               （递归调用）
+     */
     public static void preOrderTraversal(TreeNode root) {
         if (root != null) {
             System.out.println(root.data);
@@ -17,6 +23,9 @@ public class TreeNodeTraversal {
         }
     }
 
+    /**
+     * 中序遍历（左子树、根、右子树）               （递归调用）
+     */
     public static void inOrderTraversal(TreeNode root) {
         if (root != null) {
             inOrderTraversal(root.left);
@@ -25,11 +34,35 @@ public class TreeNodeTraversal {
         }
     }
 
+    /**
+     * 后序遍历（左子树、右子树、根）               （递归调用）
+     */
     public static void postOrderTraversal(TreeNode root) {
         if (root != null) {
             postOrderTraversal(root.left);
             postOrderTraversal(root.right);
             System.out.println(root.data);
+        }
+    }
+
+    /**
+     * 层次遍历（从上外下、从左往右、一层一层来）    （借助队列实现）
+     */
+    public static void levelOrderTraveral(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while (!nodeQueue.isEmpty()) {
+            TreeNode currentNode = nodeQueue.remove();
+            System.out.println(currentNode.data);
+            if (currentNode.left != null) {
+                nodeQueue.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                nodeQueue.add(currentNode.right);
+            }
         }
     }
 
