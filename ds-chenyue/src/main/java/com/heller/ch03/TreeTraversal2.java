f88182d509e606298b0d4c6179b2f7994223e277
castlebin <castlebin@gmail.com>
2018-08-26 18:25:07 +0800

    二叉树的 前序遍历  非递归实现（借助栈）

--- ds-chenyue/src/main/java/com/heller/ch03/TreeTraversal2.java
+++ ds-chenyue/src/main/java/com/heller/ch03/TreeTraversal2.java
@@ -16,7 +16,20 @@ public class TreeTraversal2 {
      * 前序遍历（根、左子树、右子树）  （非递归实现  借助栈）
      */
     public static void preOrderTraversal(TreeNode root) {
-
+        TreeNode currentNode = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (currentNode != null || !stack.isEmpty()) {
+            while (currentNode != null) {// 第一次遇到元素
+                stack.push(currentNode);
+                System.out.println(currentNode.data);
+                currentNode = currentNode.left;     // 一直往左走，直到没有左儿子了
+            }
+            if (!stack.isEmpty()) {
+                // 第二次遇到元素
+                currentNode = stack.pop();
+                currentNode = currentNode.right;    // 看看有没有右子树需要处理
+            }
+        }
     }
 
     /**
