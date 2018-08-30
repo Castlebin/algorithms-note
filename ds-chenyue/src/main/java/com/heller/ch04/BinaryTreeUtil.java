f6e78d78f4d456344b1a58348c5c71d4810c9c98
castlebin <castlebin@gmail.com>
2018-08-31 00:14:26 +0800

    实现二叉搜索树的几种基本操作 find、findMin、findMax、insert、delete

--- ds-chenyue/src/main/java/com/heller/ch04/BinaryTreeUtil.java
+++ ds-chenyue/src/main/java/com/heller/ch04/BinaryTreeUtil.java
@@ -1,5 +1,12 @@
 package com.heller.ch04;
 
+/**
+ * 实现二叉搜索树的几种基本操作
+ *
+ * find、findMin、findMax、insert、delete
+ *
+ * delete操作稍微复杂一点点，因为删除必须要考虑，删除后，要仍然是一颗二叉搜索树
+ */
 public class BinaryTreeUtil {
 
 
