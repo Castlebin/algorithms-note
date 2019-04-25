1d75c616d53f673e2a08d7f5954d841e2259023f
castlebin <castlebin@gmail.com>
2019-04-25 22:31:10 +0800

    9.1 简单排序（插入排序）

--- ds-chenyue/src/main/java/com/heller/ch09/InsertSort.java
+++ ds-chenyue/src/main/java/com/heller/ch09/InsertSort.java
@@ -11,17 +11,17 @@ public class InsertSort {
 
     public void sort(int[] array) {
         int swapCount = 0;
-        for (int i = 1; i < array.length; i++) {
+        for (int p = 1; p < array.length; p++) {
             // 新牌先拿到手里
-            int tmp = array[i];
+            int tmp = array[p];
             // 与现有的排序好的牌进行比较，从右到左，给新牌找个合适的位置插进去
-            int j = i - 1;
-            for (; j >= 0 && array[j] > tmp; j--) {
+            int i = p;
+            for (; i > 0 && array[i - 1] > tmp; i--) {
                 // 将牌往后移，移出一个空位
-                array[j + 1] = array[j];
+                array[i] = array[i - 1];
                 swapCount++;
             }
-            array[j + 1] = tmp;
+            array[i] = tmp;
         }
 
         System.out.println("InsertSort 经过交换次数 swapCount = " + swapCount);
