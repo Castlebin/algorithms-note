fd5060b099872b86d7c80eee037b3ca65bc2cfaa
castlebin <castlebin@gmail.com>
2019-04-28 00:16:56 +0800

    9.4 快速排序

--- ds-chenyue/src/main/java/com/heller/ch09/QuickSort.java
+++ ds-chenyue/src/main/java/com/heller/ch09/QuickSort.java
@@ -6,26 +6,64 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static com.heller.ArrayUtil.generateNonnegativeArray;
+import static com.heller.ArrayUtil.swap;
 
 public class QuickSort {
+
+    private int cutoff = 3;
 
     public void sort(int[] array) {
         quickSort(array, 0, array.length - 1);
     }
 
     private void quickSort(int[] array, int left, int right) {
+        if (left + cutoff <= right) {
+            int pivot = middleThree(array, left, right);
+            int i = left;
+            int j = right - 1;
+            for (; ; ) {
+                while (array[++i] < pivot) {
+                }
+                while (array[--j] > pivot) {
+                }
+                if (i < j) {
+                    swap(array, i, j);
+                } else {
+                    break;
+                }
+            }
+            swap(array, i, right - 1);
+            quickSort(array, left, i - 1);
+            quickSort(array, i + 1, right);
+        } else {
+            insertSort(array, left, right);
+        }
+    }
 
+    private void insertSort(int[] array, int left, int right) {
+        for (int p = left + 1; p <= right; p++) {
+            int tmp = array[p];
+            int i = p;
+            for (; i > left && array[i - 1] > tmp; i--) {
+                array[i] = array[i - 1];
+            }
+            array[i] = tmp;
+        }
     }
 
     private int middleThree(int[] array, int left, int right) {
-        int middle = (left + right) / 2;
-        if (array[left] <= array[middle] && array[middle] <= array[right]) {
-            return middle;
-        } else if (array[middle] <= array[left] && array[left] <= array[right]) {
-            return left;
-        } else {
-            return right;
+        int center = (left + right) / 2;
+        if (array[left] > array[center]) {
+            swap(array, left, center);
         }
+        if (array[left] > array[right]) {
+            swap(array, left, right);
+        }
+        if (array[center] > array[right]) {
+            swap(array, center, right);
+        }
+        swap(array, center, right - 1);
+        return array[right - 1];
     }
 
     @Test
@@ -33,7 +71,7 @@ public class QuickSort {
         QuickSort quickSort = new QuickSort();
         InsertSort insertSort = new InsertSort();
         for (int i = 0; i < 10; i++) {
-            int[] array = generateNonnegativeArray(100, 10000);
+            int[] array = generateNonnegativeArray(1000, 1000000);
             int[] copy = Arrays.copyOfRange(array, 0, array.length);
             int[] copy2 = Arrays.copyOfRange(array, 0, array.length);
             System.out.println("origin: " + Arrays.toString(array));
