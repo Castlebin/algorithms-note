2a2310b6f88aca05ef35eec984e82c26d260d8a8
heller <heller@gmail.com>
2019-04-28 17:36:40 +0800

    快速排序  取左边第一个作为pivot 另一种写法

---
+++ ds-chenyue/src/main/java/com/heller/ch09/QuickSort3.java
@@ -0,0 +1,62 @@
+package com.heller.ch09;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static com.heller.ArrayUtil.generateNonnegativeArray;
+import static com.heller.ArrayUtil.swap;
+
+public class QuickSort3 {
+
+    public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(array, left, right);
+            quickSort(array, left, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, right);
+        }
+    }
+
+    private int partition(int[] array, int left, int right) {
+        // 设定基准值（pivot）, 取左边第一个作为pivot （实际上表现很差的选择）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (array[i] < array[pivot]) {
+                if (i != index) {
+                    swap(array, i, index);
+                }
+                index++;
+            }
+        }
+        swap(array, pivot, index - 1);
+        return index - 1;
+    }
+
+    @Test
+    public void testSort() {
+        QuickSort3 quickSort = new QuickSort3();
+        InsertSort insertSort = new InsertSort();
+        for (int i = 0; i < 10; i++) {
+            int[] array = generateNonnegativeArray(11, 30);
+            int[] copy = Arrays.copyOfRange(array, 0, array.length);
+            int[] copy2 = Arrays.copyOfRange(array, 0, array.length);
+            System.out.println("origin: " + Arrays.toString(array));
+            System.out.println("copy  : " + Arrays.toString(array));
+
+            quickSort.sort(array);
+            insertSort.sort(copy2);
+            System.out.println("sorted: " + Arrays.toString(array));
+            Arrays.sort(copy);
+
+            Assert.assertArrayEquals("相等", array, copy);
+            Assert.assertArrayEquals("相等", array, copy2);
+        }
+    }
+
+}
