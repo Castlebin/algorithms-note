2a6ec933a2cef81443f484d41dcff360d65f86c5
heller <heller@gmail.com>
2019-10-09 15:36:51 +0800

    Java 继承与初始化

--- java-test/src/main/java/test/Parent.java
+++ java-test/src/main/java/test/Parent.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Parent {
 
@@ -15,7 +16,7 @@ public class Parent {
 
     static class Child extends Parent {
         private int i = 0;
-        private ArrayList<Integer> list;
+        private List<Integer> list;
 
         public Child() {
             super();
@@ -34,7 +35,10 @@ public class Parent {
     }
 
     /**
-     * 以下是输出内容，不太能理解最后一行输出（明白了，super()执行后，开始执行属性的赋值操作了，有int i = 0，而list没有再赋值，所以保持原状了）
+     * 以下是输出内容，不太能理解最后一行输出
+     （明白了，super()执行后，开始执行属性的赋值操作了，有private int i = 0，而list没有再赋值，所以保持原状了，
+     如果改为private List<Integer> list=new ArrayList<>()，那么最后一句中size=0，
+     同理如果改为private List<Integer> list=null，那么会报NullPointException）
      new Parent
      Parent.init
      new Child, i=100, list.size=1
