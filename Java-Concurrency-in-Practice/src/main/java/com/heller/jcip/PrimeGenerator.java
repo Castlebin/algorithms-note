8987afa81b0259ef1b3dca9ad310419552a12beb
castlebin <castlebin@gmail.com>
2019-11-16 15:20:04 +0800

    一个运行一秒钟就结束的素数生成器示例

--- Java-Concurrency-in-Practice/src/main/java/com/heller/jcip/PrimeGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.heller.jcip;
-
-public class PrimeGenerator {
-    private long nTh = 1;
-
-    public long nextPrime() {
-        return PrimeNumberGenerator.findPrimeNumber(nTh++);
-    }
-}
