5c4179c6d2b6d0ddfe643512253df746b5e19f6a
castlebin <castlebin@gmail.com>
2019-11-21 00:08:35 +0800

    模拟的CAS原子操作

---
+++ Java-Concurrency-in-Practice/src/main/java/com/heller/jcip/ch15/SimulateCAS.java
@@ -0,0 +1,24 @@
+package com.heller.jcip.ch15;
+
+import net.jcip.annotations.GuardedBy;
+
+public class SimulateCAS {
+    @GuardedBy("this") private int value;
+
+    public synchronized int getValue() {
+        return value;
+    }
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if (oldValue == expectedValue) {
+            value = newValue;
+        }
+        return oldValue;
+    }
+
+    public synchronized boolean compareAndSet(int expectedValue, int newValue) {
+        return expectedValue == compareAndSwap(expectedValue, newValue);
+    }
+
+}
