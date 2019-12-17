a1f541fc02ad0d869cc143f06ae917db35866215
heller <heller@gmail.com>
2019-12-17 19:39:23 +0800

    按照Guava RateLimiter的原理，实现一个简单的限流器 简化模型，该限流器 令牌桶大小为 1，限制流速为 1请求 每秒 关键方法在于，reserve()中计算下一次发放令牌的时间

--- Java-Concurrency-in-Practice/src/main/java/geekbang/ch04/SimpleLimiter.java
+++ Java-Concurrency-in-Practice/src/main/java/geekbang/ch04/SimpleLimiter.java
@@ -27,7 +27,8 @@ public class SimpleLimiter {
      */
     private long interval = 1_000_000_000;
 
-    synchronized long reserve(long now) {
+    // 预占令牌，返回能够获取令牌的时间
+    private synchronized long reserve(long now) {
         // 将请求令牌的时间和现在的下一次发放令牌的时间做比较
         // 如果请求时间在下一次发放令牌的时间之后，重新计算下一次发放令牌的时间
         if (now > next) {
