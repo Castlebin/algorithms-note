0a422c7c4af8b817d83fe9cf77bb26a9f509cd40
heller <heller@heller.com>
2020-03-23 23:52:44 +0800

    简单的使用 Mybatis-Plus 的例子

---
+++ spring-boot-demo/src/main/java/com/heller/sbd/entiry/User.java
@@ -0,0 +1,15 @@
+package com.heller.sbd.entiry;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+
+}
---
+++ spring-boot-demo/src/main/java/com/heller/sbd/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.heller.sbd.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heller.sbd.entiry.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
