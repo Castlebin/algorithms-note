a8bc0db70d27de5f4a1e35635d223266a855841f
heller <heller@heller.com>
2020-05-02 19:11:46 +0800

    04-树6 Complete Binary Search Tree (30分)  (未完成)

---
+++ ds-chenyue/src/main/java/ds/ch04/exe/CompleteBinarySearchTree.java
@@ -0,0 +1,15 @@
+package ds.ch04.exe;
+
+import java.util.*;
+
+/**
+ * 04-树6 Complete Binary Search Tree (30分)
+ */
+public class CompleteBinarySearchTree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numCount = Integer.parseInt(sc.nextLine());
+
+    }
+
+}
