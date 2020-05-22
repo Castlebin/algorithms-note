e7d988d9d0365c0c1ed177c1b13fff7b3150fd0e
heller <heller@heller.com>
2020-05-23 01:22:34 +0800

    staticObj、instanceObj、localObj存放在哪里？

---
+++ jvm-deep-thinking/src/main/java/ch04/JHSDBTestCase.java
@@ -0,0 +1,30 @@
+package ch04;
+
+/**
+ * staticObj、instanceObj、localObj存放在哪里？
+
+ JDK 11
+ vm args:
+
+    -Xmx10m -XX:+UseSerialGC -XX:+UseCompressedOops
+
+ */
+public class JHSDBTestCase {
+
+    static class Test {
+        static ObjectHolder staticObj = new ObjectHolder();
+        ObjectHolder instanceObj = new ObjectHolder();
+
+        void foo() {
+            ObjectHolder localObj = new ObjectHolder();
+            System.out.println("done");    // 这里设一个断点
+        }
+    }
+
+    private static class ObjectHolder {}
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.foo();
+    }
+}
