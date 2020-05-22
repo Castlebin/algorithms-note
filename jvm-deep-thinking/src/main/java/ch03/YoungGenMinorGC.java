7f85b97a816e8d83aa51d90a5f0b5768a3cf222e
heller <heller@heller.com>
2020-05-22 17:59:12 +0800

    内存分配策略

--- jvm-deep-thinking/src/main/java/ch03/YoungGenMinorGC.java
+++ jvm-deep-thinking/src/main/java/ch03/YoungGenMinorGC.java
@@ -1,9 +1,12 @@
 package ch03;
 
 /**
- * 测试新生代的Minor GC
+ * 测试新生代的Minor GC  JDK 8
  * VM args:
- *      -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+
+       -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps
+
+ -XX:+PrintGCTimeStamps 显示的是jvm启动时间，还是PrintGCDateStamps比较直观点
  */
 public class YoungGenMinorGC {
     public static void main(String[] args) {
@@ -15,8 +18,15 @@ public class YoungGenMinorGC {
     public static void testAllocation() {
         byte[] a1, a2, a3, a4;
         a1 = new byte[2 * _1M];
+        System.out.println("allocate a1 success..");
+
         a2 = new byte[2 * _1M];
+        System.out.println("allocate a2 success..");
+
         a3 = new byte[2 * _1M];
-        a4 = new byte[3 * _1M];
+        System.out.println("allocate a3 success..");
+
+        a4 = new byte[3 * _1M];     // 出现一次Minor GC
+        System.out.println("allocate a4 success..");
     }
 }
