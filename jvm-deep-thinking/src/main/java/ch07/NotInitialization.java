a3e77d39ee5a9c009e83b3b2ff0d5c76c9fb5491
heller <heller@heller.com>
2020-05-23 18:27:19 +0800

    非主动使用类字段演示，不触发类的初始化

---
+++ jvm-deep-thinking/src/main/java/ch07/NotInitialization.java
@@ -0,0 +1,10 @@
+package ch07;
+
+/**
+ * 非主动使用类字段演示
+ **/
+public class NotInitialization {
+    public static void main(String[] args) {
+        System.out.println(SubClass.value);
+    }
+}
---
+++ jvm-deep-thinking/src/main/java/ch07/SubClass.java
@@ -0,0 +1,7 @@
+package ch07;
+
+public class SubClass extends SuperClass {
+    static {
+        System.out.println("SubClass init!");
+    }
+}
