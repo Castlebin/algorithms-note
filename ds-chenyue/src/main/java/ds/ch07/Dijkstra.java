7572ff61cefe8c2c36b5d7be7e8f19b09db6bb78
heller <heller@heller.com>
2020-06-09 17:37:02 +0800

    有权图 单源最短路径 - Dijkstra 算法   todo

--- ds-chenyue/src/main/java/ds/ch07/Dijkstra.java
+++ ds-chenyue/src/main/java/ds/ch07/Dijkstra.java
@@ -1,9 +1,36 @@
 package ds.ch07;
 
+import ds.ch06.GraphLinkedList;
+
 /**
-    单源最短路径 - Dijkstra 算法
+    有权图 单源最短路径 - Dijkstra 算法
 
     以递增（非递减）的顺序，收录到原点距离最短的每个订单
+
+ todo
  */
 public class Dijkstra {
+
+    public DistPath[] dijkstra(GraphLinkedList graph, int s) {
+        DistPath[] result = new DistPath[graph.numOfVertex];
+        initialDataPath(result);
+
+        return result;
+    }
+
+    private void initialDataPath(DistPath[] result) {
+        for (DistPath distPath : result) {
+            if (distPath == null) {
+                distPath = new DistPath();
+            }
+            distPath.dist = Short.MAX_VALUE;
+            distPath.path = -1;
+        }
+    }
+
+    class DistPath {
+        public int dist;
+        public int path;
+    }
+
 }
