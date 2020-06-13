b40cc9dd6133daa8e6df5662d244a0e242d799a5
heller <heller@heller.com>
2020-06-13 21:36:06 +0800

    11. 旋转数组的最小数字
     三个数组都相同时，没办法，只能退化为顺序查找

---
+++ leetcode/src/main/java/sword/S010.java
@@ -0,0 +1,53 @@
+package sword;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ 11. 旋转数组的最小数字
+
+ 三个数组都相同时，没办法，只能退化为顺序查找
+ */
+public class S010 {
+
+    public int minArray(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            throw new RuntimeException("输入不正确");
+        }
+        int begin = 0, end = numbers.length - 1;
+        if (numbers[begin] < numbers[numbers.length - 1]) {
+            return numbers[begin];
+        }
+        int mid;
+        while (begin < end - 1) {
+            mid = (begin + end) / 2;
+            // 三个数组都相同时，没办法，只能退化为顺序查找
+            if (numbers[begin] == numbers[mid]
+                && numbers[mid] == numbers[end]) {
+                return findMinInOrder(numbers, begin, end);
+            } else if (numbers[mid] >= numbers[begin]) {
+                begin = mid;
+            } else if (numbers[mid] <= numbers[end]) {
+                end = mid;
+            }
+        }
+
+        return numbers[end];
+    }
+
+    private int findMinInOrder(int[] numbers, int begin, int end) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1, minArray(new int[]{3,1,3}));
+    }
+
+}
