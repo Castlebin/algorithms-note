49edf175f0a4a6460e2dc7d18f2a1bf23ddc6bc4
heller <heller@heller.com>
2020-06-14 23:34:56 +0800

    面试题22. 链表中倒数第k个节点
     输入一个链表，输出该链表中倒数第k个节点。
    
     为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
     例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。
     这个链表的倒数第3个节点是值为4的节点。

---
+++ leetcode/src/main/java/sword/S022.java
@@ -0,0 +1,50 @@
+package sword;
+
+import org.junit.Test;
+
+/**
+ 面试题22. 链表中倒数第k个节点
+
+ 输入一个链表，输出该链表中倒数第k个节点。
+
+ 为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ 例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。
+ 这个链表的倒数第3个节点是值为4的节点。
+ */
+public class S022 {
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        if (k <= 0) {
+            throw new IllegalArgumentException("k must >= 1");
+        }
+        if (head == null) {
+            return null;
+        }
+        int length = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            length++;
+            cur = cur.next;
+        }
+        if (k > length) {
+            return null;
+        }
+        // 倒数第 k 个，不就是 顺数 第 length + 1 - k 个吗？
+        cur = head;
+        int count = 0;
+        while (cur != null) {
+            count++;
+            if (count == length + 1 - k) {
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
