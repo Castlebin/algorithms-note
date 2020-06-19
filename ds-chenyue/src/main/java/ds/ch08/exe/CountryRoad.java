c1d15209a019da16a87db90d6d37976c46ddfcb8
heller <heller@heller.com>
2020-06-19 20:09:25 +0800

    08-图7 公路村村通 (30分)  现有村落间道路的统计数据表中，列出了有可能建设成标准公路的若干条道路的成本，  求使每个村落都有公路连通所需要的最低成本。
     输入格式:
     输入数据包括城镇数目正整数N（≤1000）和候选道路数目M（≤3N）；
     随后的M行对应M条道路，每行给出3个正整数，分别是该条道路直接连通的两个城镇的编号以及该道路改建的预算成本。
     为简单起见，城镇从1到N编号。
    
     输出格式:
     输出村村通需要的最低成本。如果输入数据不足以保证畅通，则输出−1，表示需要建设更多公路。
    
     Prim 算法

--- ds-chenyue/src/main/java/ds/ch08/exe/CountryRoad.java
+++ ds-chenyue/src/main/java/ds/ch08/exe/CountryRoad.java
@@ -1,9 +1,6 @@
 package ds.ch08.exe;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  08-图7 公路村村通 (30分)
@@ -26,58 +23,97 @@ public class CountryRoad {
      * 方法1. 邻接矩阵 Prim 算法
      */
     public static void main(String[] args) {
-        Graph graph = buildGraphFromInput();
+        int[][] graph = buildGraphFromInput();
         int[] mst = prim(graph, 0);
+        if (mst == null) {
+            System.out.println(-1);
+        } else {
+            int roadLength = 0;
+            for (int v = 0; v < graph.length; v++) {
+                if (mst[v] != -1) {
+                    roadLength += graph[v][mst[v]];
+                }
+            }
+            System.out.println(roadLength);
+        }
     }
 
-    private static int[] prim(Graph graph, int source) {
-        int countries = graph.graph.length;
+    private static int[] prim(int[][] graph, int source) {
+        int countries = graph.length;
         int count = 0;
-        int[] dist = initDist(graph.graph, source);
-        int[] mst = new int[countries];
-
-        mst[0] = -1;
-        count++;
+        int[] dist = initDist(graph, source);
+        int[] mst = initMst(graph, source);
+        int v = source;
         while (true) {
-            // todo
-            break;
-        }
+            // 将 v 收录
+            dist[v] = 0;
+            count++;
+            // 更新 v 的 未收录的邻接点 w 的 dist
+            for (int w = 0; w < countries; w++) {
+                if (dist[w] != 0 && dist[w] > graph[v][w]) {
+                    dist[w] = graph[v][w];
+                    mst[w] = v;
+                }
+            }
 
-        return null;
+            // 返回未收录顶点中 dist 值最小的
+            v = findMinDistVertex(dist, mst, source);
+            if (v == -1) {
+                break;
+            }
+        }
+        if (count < countries - 1) {
+            // 图不连通
+            return null;
+        }
+        return mst;
+    }
+
+    private static int[] initMst(int[][] graph, int source) {
+        int[] mst = new int[graph.length];
+        for (int v = 0; v < graph.length; v++) {
+            if (graph[source][v] != INF) {
+                mst[v] = source;
+            }
+        }
+        return mst;
+    }
+
+    private static int findMinDistVertex(int[] dist, int[] mst, int source) {
+        int d = INF;
+        int minDistVertex = -1;
+        for (int v = 0; v < dist.length; v++) {
+            if (dist[v] != 0 && dist[v] < d) {
+                d = dist[v];
+                minDistVertex = v;
+            }
+        }
+        return minDistVertex; // 表示不存在这样的点
     }
 
     private static int[] initDist(int[][] graph, int source) {
-        int countries = graph.length;
-        int[] dist = new int[countries];
-        for (int to = 0; to < countries; to++) {
-            dist[to] = graph[source][to];
+        int[] dist = new int[graph.length];
+        for (int v = 0; v < graph.length; v++) {
+            dist[v] = graph[source][v];
         }
-
         return dist;
     }
 
-    public static Graph buildGraphFromInput() {
-        Graph graph = new Graph();
+    public static int[][] buildGraphFromInput() {
         Scanner sc = new Scanner(System.in);
         String firstLine = sc.nextLine();
         String[] metaData = firstLine.split("\\s+");
         int countries = Integer.parseInt(metaData[0]);
         int roads = Integer.parseInt(metaData[1]);
 
-        graph.graph = initGraph(countries);
+        int[][] graph = initGraph(countries);
         for (int i = 0; i < roads; i++) {
             String[] road = sc.nextLine().split("\\s+");
             int from = Integer.parseInt(road[0]) - 1;
             int to = Integer.parseInt(road[1]) - 1;
             int weight = Integer.parseInt(road[2]);
-            graph.graph[from][to] = weight;
-            graph.graph[to][from] = weight;
-
-            Edge edge = new Edge();
-            edge.from = from;
-            edge.to = to;
-            edge.weight = weight;
-            graph.edgeHeap.add(edge);
+            graph[from][to] = weight;
+            graph[to][from] = weight;
         }
         return graph;
     }
@@ -95,16 +131,6 @@ public class CountryRoad {
             }
         }
         return graph;
-    }
-
-    static class Graph {
-        public int[][] graph;
-        public PriorityQueue<Edge> edgeHeap = new PriorityQueue<>(new Comparator<Edge>() {
-            @Override
-            public int compare(Edge o1, Edge o2) {
-                return o1.weight - o2.weight;
-            }
-        });
     }
 
     static class Edge {
