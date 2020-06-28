4b42843398531b02aa9c127d478428ccc08526e1
heller <heller@heller.com>
2020-06-28 21:53:50 +0800

    剑指 Offer 40. 最小的k个数
     输入整数数组 arr ，找出其中最小的 k 个数。
     例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。

---
+++ leetcode/src/main/java/sword/S040.java
@@ -0,0 +1,46 @@
+package sword;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ 剑指 Offer 40. 最小的k个数
+
+ 输入整数数组 arr ，找出其中最小的 k 个数。
+ 例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ */
+public class S040 {
+
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (arr == null || arr.length == 0 || arr.length <= k) {
+            return arr;
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for (int item : arr) {
+            if (heap.size() <= k) {
+                heap.add(item);
+            }
+            if (heap.size() > k) {
+                heap.remove();
+            }
+        }
+        int[] result = new int[heap.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = heap.remove();
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
