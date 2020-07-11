bd2b9653635d6bd3279c0afe31c2df3fee2f1056
heller <heller@heller.com>
2020-07-11 18:33:38 +0800

    快速排序 与 基于三向切分的快速排序算法

---
+++ ds-chenyue/src/main/java/ds/sort/QuickSort.java
@@ -0,0 +1,56 @@
+package ds.sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class QuickSort<T extends Comparable<T>> extends Sort<T> {
+
+    @Override
+    public void sort(T[] nums) {
+        shuffle(nums);
+        sort(nums, 0, nums.length - 1);
+    }
+
+    protected void sort(T[] nums, int l, int h) {
+        if (h <= l) {
+            return;
+        }
+        int j = partition(nums, l, h);
+        sort(nums, l, j - 1);
+        sort(nums, j + 1, h);
+    }
+
+    private void shuffle(T[] nums) {
+        List<Comparable> list = Arrays.asList(nums);
+        Collections.shuffle(list);
+        list.toArray(nums);
+    }
+
+    private int partition(T[] nums, int l, int h) {
+        int i = l, j = h + 1;
+        T v = nums[l];
+        while (true) {
+            while (less(nums[++i], v) && i != h);
+            while (less(v, nums[--j]) && j != l);
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    @Test
+    public void testSort() {
+        Integer[] arr = new Integer[]{4, 981, 10, -17, 0, -20, 29, 50, 8, 43, -5};
+        Sort<Integer> sort = new QuickSort<>();
+        sort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
+
