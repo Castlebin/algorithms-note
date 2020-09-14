ba132846d9a75b53c4b0b984dbbdfc2f877fdf36
heller <heller@heller.com>
2020-09-14 22:03:11 +0800

    rm demos

--- spring-boot-demo/src/main/java/com/heller/sbd/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.heller.sbd.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.heller.sbd.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
