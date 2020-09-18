bf65fe2e3951afa49f5d9efcf446edbed58e90e0
heller <heller@gmail.com>
2020-09-18 18:33:18 +0800

    二进制 运算 相关题目

--- leetcode/src/main/java/sword/S015.java
+++ leetcode/src/main/java/sword/S015.java
@@ -47,3 +47,18 @@ public class S015 {
     }
 
 }
+
+
+/**
+ 相关题目：
+ 0. 一个整型数组 nums 里除 1 个数字之外，其他数字都出现了两次。请写程序找出这 1 个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+
+ 1. 一个整型数组 nums 里除 2 个数字之外，其他数字都出现了两次。请写程序找出这 两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+    剑指 Offer 56 - I. 数组中数字出现的次数
+    https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/
+
+ 3. 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了 三 次。请找出那个只出现一次的数字。
+     剑指 Offer 56 - II. 数组中数字出现的次数 II
+     https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/
+
+ */
