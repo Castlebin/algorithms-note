9049fc83c63620c6b3008f638a3b4b095c803fbd
heller <heller@heller.com>
2020-09-22 00:57:44 +0800

    剑指 Offer 28. 对称的二叉树
     请实现一个函数，用来判断一棵二叉树是不是对称的。
     如果一棵二叉树和它的镜像一样，那么它是对称的。

--- leetcode/src/main/java/sword/S028.java
+++ leetcode/src/main/java/sword/S028.java
@@ -54,6 +54,30 @@ public class S028 {
                 && left == null && right == null;
     }
 
+    /**
+     * 递归解法，简洁
+     */
+    class Solution2 {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return isSymmetric(root.left, root.right);
+        }
+
+        public boolean isSymmetric(TreeNode left, TreeNode right) {
+            if (left == null && right == null) {
+                return true;
+            } else if (left != null && right != null) {
+                return left.val == right.val
+                        && isSymmetric(left.left, right.right)
+                        && isSymmetric(left.right, right.left);
+            } else {
+                return false;
+            }
+        }
+    }
+
     @Test
     public void test() {
         TreeNode root = new TreeNode(1){{
