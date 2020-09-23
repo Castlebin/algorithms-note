e5c8d3e340265e75aeee5c3679c1303e11345c33
heller <heller@heller.com>
2020-09-23 21:40:57 +0800

    面试题25. 合并两个排序的链表  输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
     递归实现，太优雅了！代码简洁太多

--- leetcode/src/main/java/sword/S025.java
+++ leetcode/src/main/java/sword/S025.java
@@ -9,6 +9,28 @@ import common.ListNode;
  */
 public class S025 {
 
+    /**
+     * 递归实现，更简洁，太优雅了！
+     */
+    class Solution2 {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            } else if (l2 == null) {
+                return l1;
+            }
+            ListNode result;
+            if (l1.val < l2.val) {
+                result = l1;
+                result.next = mergeTwoLists(l1.next, l2);
+            } else {
+                result = l2;
+                result.next = mergeTwoLists(l1, l2.next);
+            }
+            return result;
+        }
+    }
+
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
