cf9495ec61317e096cf694f786f49510edc84c19
heller <heller@heller.com>
2020-09-27 00:42:09 +0800

    剑指 Offer 60. n个骰子的点数  把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
     你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。

--- leetcode/src/main/java/sword/S060.java
+++ leetcode/src/main/java/sword/S060.java
@@ -2,6 +2,7 @@ package sword;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 
 /**
  剑指 Offer 60. n个骰子的点数
@@ -18,6 +19,65 @@ import org.junit.Test;
  输出: [0.02778,0.05556,0.08333,0.11111,0.13889,0.16667,0.13889,0.11111,0.08333,0.05556,0.02778]
 */
 public class S060 {
+
+    /**
+     * 使用两个数组，保存上一轮、本轮，每个可能点数的次数，要概率的话，除以总可能次数即可
+     *
+     * arr[i] 表示 和为点数 i 的骰子组合次数
+     *
+     * 此方法容易理解，计算简单
+     */
+
+    // 表示 每个骰子 有 1-6 这几个值
+    public static final int DICE_FACES = 6;
+
+    public double[] probability(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must >= 1");
+        }
+
+        int maxSum = DICE_FACES * n;
+        int[] probabilities = new int[maxSum + 1];
+        for (int i = 1; i <= DICE_FACES; i++) {
+            probabilities[i] = 1;
+        }
+        if (n > 1) {
+            // 为了节约频繁申请数组的开销，所以申请了两个数组，重复使用。注意处理
+            int[] lastProbabilities = new int[maxSum + 1];
+            for (int diceCount = 2; diceCount <= n; diceCount++) {
+                int[] tmp = lastProbabilities;
+                lastProbabilities = probabilities;
+                probabilities = tmp;
+                // 注意处理上一轮遗留的计数
+                probabilities[diceCount - 2] = 0;
+                probabilities[diceCount - 1] = 0;
+                int maxNum = diceCount * DICE_FACES;
+                for (int index = diceCount; index <= maxNum; index++) {
+                    // 记住要先重置为0
+                    probabilities[index] = 0;
+                    for (int face = 1; face <= DICE_FACES && index - face >= 0; face++) {
+                        probabilities[index] += lastProbabilities[index - face];
+                    }
+                }
+            }
+        }
+        double total = Math.pow(6, n);
+
+        double[] p = new double[5 * n + 1];
+        for(int i = n,t = 0; i < probabilities.length; i++) {
+            p[t++] = probabilities[i] / total;
+        }
+
+        return p;
+
+        // 下面的lambda 表达式很慢，用循环好了
+        /*
+        return Arrays.stream(probabilities)
+                .filter( x -> x > 0.0 )
+                .mapToDouble( x -> x / total)
+                .toArray();*/
+    }
+
 
     /**
      * todo
@@ -41,7 +101,9 @@ public class S060 {
 
     @Test
     public void test() {
-
+        System.out.println(Arrays.toString(probability(1)));
+        System.out.println(Arrays.toString(probability(2)));
+        System.out.println(Arrays.toString(probability(3)));
     }
 
 }
