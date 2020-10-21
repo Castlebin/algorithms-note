a0e1138b3766c4c25dd52c70819bf70bc933984d
heller <heller@heller.com>
2020-10-21 17:34:34 +0800

    剑指 Offer 19. 正则表达式匹配
     请实现一个函数用来匹配包含'. '和'*'的正则表达式。
     模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）。
     在本题中，匹配是指字符串的所有字符匹配整个模式。
     例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
    
     迭代算法，去掉了一个多余的迭代，速度大大加快！！
     动态规划算法，最快 才 5ms！！

--- leetcode/src/main/java/sword/S019.java
+++ leetcode/src/main/java/sword/S019.java
@@ -10,6 +10,8 @@ import org.junit.Test;
  模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）。
  在本题中，匹配是指字符串的所有字符匹配整个模式。
  例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
+ 
+ （动态规划，最快）
  */
 public class S019 {
 
@@ -68,7 +70,7 @@ public class S019 {
     }
 
     /**
-     * 动态规划，非常快
+     * 动态规划，非常快，5 ms！！
      */
     class Solution {
         public boolean isMatch(String str, String pattern) {
