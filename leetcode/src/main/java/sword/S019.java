e507c05817fd2239e21db0219c28e3bddcbb65e6
heller <heller@heller.com>
2020-10-21 22:48:45 +0800

    剑指 Offer 19. 正则表达式匹配
     请实现一个函数用来匹配包含'. '和'*'的正则表达式。
     模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）。
     在本题中，匹配是指字符串的所有字符匹配整个模式。
     例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
    
     （动态规划，最快）

--- leetcode/src/main/java/sword/S019.java
+++ leetcode/src/main/java/sword/S019.java
@@ -16,6 +16,47 @@ import org.junit.Test;
 public class S019 {
 
     /**
+     * 同样的 动态规划 方法
+     */
+    class Solution4 {
+        public boolean isMatch(String s, String p) {
+            if (s == null || p == null) {
+                return false;
+            }
+            int m = s.length();
+            int n = p.length();
+            boolean[][] dp = new boolean[m + 1][n + 1];
+            dp[0][0] = true;
+            for (int i = 0; i < n; i++) {
+                //如果p的第i+1个字符也就是p.charAt(i)是"*"的话，
+                //那么他就可以把p的第i个字符给消掉（也就是匹配0次）。
+                //我们只需要判断p的第i-1个字符和s的前0个字符是否匹
+                //配即可。比如p是"a*b*"，如果要判断p的第4个字符
+                //"*"和s的前0个字符是否匹配，因为字符"*"可以消去
+                //前面的任意字符，只需要判断p的"a*"和s的前0个字
+                //符是否匹配即可
+                if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                    dp[0][i + 1] = true;
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.') {
+                        dp[i + 1][j + 1] = dp[i][j];
+                    } else if (p.charAt(j) == '*') {
+                        //递归公式
+                        if (p.charAt(j - 1) == s.charAt(i) || p.charAt(j - 1) == '.') {
+                            dp[i + 1][j + 1] = dp[i][j + 1];
+                        }
+                        dp[i + 1][j + 1] |= dp[i + 1][j - 1];
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+    }
+
+    /**
      * 83 ms，因为 每次都是新建数组，所以多了一点消耗，大体算法思路和 Solution1 一样的
      */
     class Solution3 {
