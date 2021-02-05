a1cd84c5dad3d66b802822e00030c3bc12b1dd10
heller <heller@heller.com>
2021-02-06 07:04:44 +0800

    不用加减乘除做加法
    // a + b
     while(b != 0)
       1. 不带进位的加法 int tmp = a ^ b;
       2. 进位 b = (a & b) << 1;
       3. a = tmp

--- leetcode/src/main/java/algorithm/NumberUtil.java
+++ leetcode/src/main/java/algorithm/NumberUtil.java
@@ -1,9 +1,32 @@
 package algorithm;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class NumberUtil {
 
     /**
-     * 不用
+     * 不用加减乘除做加法
      */
+    public long add(int a, int b) {
+        while (b != 0) {
+            // 不带进位的加法，异或
+            int tmp = a ^ b;
+            // 进位
+            b = (a & b) << 1;
+            a = tmp;
+        }
+        return a;
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 10, b = 20;
+        Assert.assertEquals(30, add(a, b));
+        a = -10; b = 20;
+        Assert.assertEquals(10, add(a, b));
+        a = 10; b = -20;
+        Assert.assertEquals(-10, add(a, b));
+    }
 
 }
