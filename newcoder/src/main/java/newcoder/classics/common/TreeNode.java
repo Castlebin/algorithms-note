3296ccb38699e979e56f606d4eae82cb19afa42f
heller <heller@heller.com>
2021-03-06 23:12:13 +0800

    剑指 Offer 40. 最小的k个数
     输入整数数组 arr ，找出其中最小的 k 个数。
     例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。

--- newcoder/src/main/java/newcoder/classics/common/TreeNode.java
+++ newcoder/src/main/java/newcoder/classics/common/TreeNode.java
@@ -8,7 +8,11 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+    }
+
     public TreeNode(int val) {
         this.val = val;
     }
+
 }
