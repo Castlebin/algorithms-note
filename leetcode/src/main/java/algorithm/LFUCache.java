2888d6f4324a271ea5a3ec18b08a78596bdf00dc
heller <heller@heller.com>
2021-03-20 13:06:34 +0800

    LFUCache

--- leetcode/src/main/java/algorithm/LFUCache.java
+++ leetcode/src/main/java/algorithm/LFUCache.java
@@ -6,83 +6,84 @@ import java.util.*;
  * LFU 缓存
  */
 public class LFUCache<K, V> {
-    // 缓存 key 的最大容量，超出的话，得做逐出
-    int capacity;
 
-    // 缓存 key - value
-    private Map<K, V> keyToValMap;
-    // 记录 Key 的访问频次
-    private Map<K, Integer> keyToFreqMap;
-    // 记录频次和缓存 key 的对应关系，同一个频次可能会有多个 key ，freq 可以看做有序
-    private TreeMap<Integer, List<K>> freqToKeysMap;
+    private int capacity;
+
+    /** 存放 缓存 key-value */
+    private Map<K, V> keyValueMap;
+
+    /** 存放 缓存 key 的访问频率对应关系 */
+    private Map<K, Integer> keyFreqMap;
+    /** 存放 访问频率 和 key 对应关系 */
+    private Map<Integer, List<K>> freqKeysMap;
+    /** 存放当前最小的访问频率 */
+    private int minFreq;
 
     public LFUCache(int capacity) {
         if (capacity <= 0) {
-            throw new IllegalArgumentException("capacity must > 0");
+            throw new IllegalArgumentException("capacity must >= 1");
         }
         this.capacity = capacity;
-        keyToValMap = new HashMap<>();
-        keyToFreqMap = new HashMap<>();
-        freqToKeysMap = new TreeMap<>();
+        keyValueMap = new HashMap<>();
+        keyFreqMap = new HashMap<>();
+        freqKeysMap = new HashMap<>();
     }
 
     public V get(K key) {
-        V value = keyToValMap.get(key);
-        // 如果存在，则应该提升 key 的访问频次
+        V value = keyValueMap.get(key);
         if (value != null) {
-            incrementKeyFreq(key);
+            // key 存在，需要更新 key 的访问频率
+            increaseKeyFreq(key);
         }
         return value;
     }
 
-    private void incrementKeyFreq(K key) {
-        // 提升频次
-        int freq = keyToFreqMap.get(key);
-        keyToFreqMap.put(key, freq + 1);
-
-        if (freqToKeysMap.get(freq + 1) == null) {
-            freqToKeysMap.put(freq + 1, new LinkedList<K>());
+    private void increaseKeyFreq(K key) {
+        int freq = keyFreqMap.get(key);
+        keyFreqMap.put(key, freq + 1);
+        if (freqKeysMap.get(freq + 1) == null) {
+            freqKeysMap.put(freq + 1, new ArrayList<>());
         }
-        freqToKeysMap.get(freq + 1).add(key);
+        freqKeysMap.get(freq + 1).add(key);
 
-        // 从 freqToKeysMap 中 当前频次 中移除该 key ，如果移除后，该 频次 对应的 key list 为空，从 freqToKeysMap 中移除该频次
-        freqToKeysMap.get(freq).remove(key);
-        if (freqToKeysMap.get(freq).size() == 0) {
-            freqToKeysMap.remove(freq);
+        freqKeysMap.get(freq).remove(key);
+        if (freqKeysMap.get(freq).size() == 0) {
+            freqKeysMap.remove(freq);
+            if (freq == minFreq) {
+                minFreq++;
+            }
         }
     }
 
     public void put(K key, V value) {
-        V exist = keyToValMap.get(key);
-        if (exist == null) {
-            // 说明是新加的一个 key
-            // 如果缓存已满，那么先做逐出
-            if (keyToValMap.size() >= capacity) {
-                removeLeastFreqKey();
-            }
-            keyToValMap.put(key, value);
-            // 新加的 key，记录它的频次
-            keyToFreqMap.put(key, 1);
-            if (!freqToKeysMap.containsKey(1)) {
-                freqToKeysMap.put(1, new LinkedList<K>());
-            }
-            freqToKeysMap.get(1).add(key);
+        V oldValue = keyValueMap.get(key);
+        if (oldValue != null) {
+            // 更新旧值，更新访问频率
+            keyValueMap.put(key, value);
+            increaseKeyFreq(key);
         } else {
-            // 已经存在的 key
-            keyToValMap.put(key, value);
-            // 提升它的频次
-            incrementKeyFreq(key);
+            // 新增一个 key
+            // 先要检查缓存是否已满，满的话，需要做逐出
+            if (keyValueMap.size() == capacity) {
+                removeLeaseFreqKey();
+            }
+            keyValueMap.put(key, value);
+            keyFreqMap.put(key, 1);
+            if (freqKeysMap.get(1) == null) {
+                freqKeysMap.put(1, new ArrayList<>());
+            }
+            freqKeysMap.get(1).add(key);
+            minFreq = 1;
         }
     }
 
-    private void removeLeastFreqKey() {
-        Integer leastFreq = freqToKeysMap.firstKey();
-        K leastFreqOldestKey = freqToKeysMap.get(leastFreq).get(0);
-        keyToValMap.remove(leastFreqOldestKey);
-        keyToFreqMap.remove(leastFreqOldestKey);
-        freqToKeysMap.get(leastFreq).remove(0);
-        if (freqToKeysMap.get(leastFreq).size() == 0) {
-            freqToKeysMap.remove(leastFreq);
+    private void removeLeaseFreqKey() {
+        K leaseFreqKey = freqKeysMap.get(minFreq).get(0);
+        keyValueMap.remove(leaseFreqKey);
+        keyFreqMap.remove(leaseFreqKey);
+        freqKeysMap.get(minFreq).remove(0);
+        if (freqKeysMap.get(minFreq).size() == 0) {
+            freqKeysMap.remove(minFreq);
         }
     }
 
