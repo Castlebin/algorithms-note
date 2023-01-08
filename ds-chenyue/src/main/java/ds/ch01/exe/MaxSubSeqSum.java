2f14dfe4f54135a7e39b48719e8a03dfca9d68ac
Castlebin <Castlebin@gmail.com>
2023-01-08 23:50:18 +0800

    删除不必要的依赖

--- ds-chenyue/src/main/java/ds/ch01/exe/MaxSubSeqSum.java
+++ ds-chenyue/src/main/java/ds/ch01/exe/MaxSubSeqSum.java
@@ -1,7 +1,5 @@
 package ds.ch01.exe;
 
-import cn.hutool.core.util.NumberUtil;
-
 import java.util.Scanner;
 
 /**
@@ -113,7 +111,13 @@ public class MaxSubSeqSum {
     }
 
     private static int max(int... nums) {
-        return NumberUtil.max(nums);
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
     }
 
     /**
