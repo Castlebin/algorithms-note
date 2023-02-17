70913029c10a796e9120e9417eee86f4af554621
Castlebin <Castlebin@gmail.com>
2023-02-18 00:54:27 +0800

    单向链表（节点）

--- leetcode/src/test/java/leetcode/base/ListNode.java
+++ leetcode/src/test/java/leetcode/base/ListNode.java
@@ -4,8 +4,8 @@ package leetcode.base;
  * 单向链表（节点）
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode(int x) {
         val = x;
