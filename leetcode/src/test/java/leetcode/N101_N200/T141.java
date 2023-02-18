a9082aec189b1c5d63b2894b8e7684c2c5307222
Castlebin <Castlebin@gmail.com>
2023-02-18 12:46:02 +0800

    141. Linked List Cycle     环形链表

--- leetcode/src/test/java/leetcode/N101_N200/T141.java
+++ leetcode/src/test/java/leetcode/N101_N200/T141.java
@@ -4,10 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import leetcode.base.ListNode;
+import leetcode.base.ListNodeUtil;
 
 /**
  * 141. Linked List Cycle     环形链表
- * <p>
+ *
  * 给你一个链表的头节点 head ，判断链表中是否有环。
  */
 public class T141 {
@@ -38,19 +39,19 @@ public class T141 {
 
     @Test
     public void test() {
-        ListNode l1 = ListNode.buildLinkedList(new int[] {3, 2, 0, -4}, 1);
+        ListNode l1 = ListNodeUtil.buildLinkedList(new int[] {3, 2, 0, -4}, 1);
         Assert.assertEquals(true, hasCycle(l1));
 
-        ListNode l2 = ListNode.buildLinkedList(new int[] {1, 2}, 0);
+        ListNode l2 = ListNodeUtil.buildLinkedList(new int[] {1, 2}, 0);
         Assert.assertEquals(true, hasCycle(l2));
 
-        ListNode l3 = ListNode.buildLinkedList(new int[] {1, 2}, -1);
+        ListNode l3 = ListNodeUtil.buildLinkedList(new int[] {1, 2}, -1);
         Assert.assertEquals(false, hasCycle(l3));
 
-        ListNode l4 = ListNode.buildLinkedList(new int[] {1}, -1);
+        ListNode l4 = ListNodeUtil.buildLinkedList(new int[] {1}, -1);
         Assert.assertEquals(false, hasCycle(l4));
 
-        ListNode l5 = ListNode.buildLinkedList(new int[] {1}, 0);
+        ListNode l5 = ListNodeUtil.buildLinkedList(new int[] {1}, 0);
         Assert.assertEquals(true, hasCycle(l5));
     }
 
