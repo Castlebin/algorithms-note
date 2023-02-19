280e90212310b5c80063363f39f4f8827a2148e2
Castlebin <Castlebin@gmail.com>
2023-02-19 17:22:49 +0800

    46. 全排列 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。

---
+++ leetcode/src/test/java/leetcode/N1_N99/T46.java
@@ -0,0 +1,54 @@
+package leetcode.N1_N99;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+
+/**
+ * 46. Permutations
+ * 46. 全排列
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ */
+public class T46 {
+
+    /**
+     * 解法1：全排列的递归解法（模拟排列组合的过程）
+     */
+    class Solution_1 {
+        public List<List<Integer>> permute(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return new ArrayList<>();
+            }
+            List<List<Integer>> res = new ArrayList<>();
+            for (int num : nums) {
+                if (res.isEmpty()) {
+                    List<Integer> init = new ArrayList<Integer>() {{add(num);}};
+                    res.add(init);
+                    continue;
+                }
+                res = permute(res, num);
+            }
+            return res;
+        }
+
+        private List<List<Integer>> permute(List<List<Integer>> res, int num) {
+            List<List<Integer>> thisRes = new ArrayList<>();
+            for (List<Integer> re : res) {
+                for (int index = 0; index <= re.size(); index++) {
+                    List<Integer> copy = new ArrayList<>(re);
+                    copy.add(index, num);
+                    thisRes.add(copy);
+                }
+            }
+            return thisRes;
+        }
+    }
+
+    @Test
+    public void test() {
+        new Solution_1().permute(new int[] {1, 2, 3});
+    }
+
+}
