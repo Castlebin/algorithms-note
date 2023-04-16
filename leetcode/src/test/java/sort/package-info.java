023ec9e5d1ce06c90ea5b136d6da816967eb9dd7
Castlebin <Castlebin@gmail.com>
2023-04-16 13:30:46 +0800

    各种排序算法练习（+测试链接）

--- leetcode/src/test/java/sort/package-info.java
+++ leetcode/src/test/java/sort/package-info.java
@@ -2,6 +2,10 @@ package sort;
 
 /**
  * 各种排序算法练习
+ * leetcode 测试: https://leetcode.cn/problems/sort-an-array/
+ *               https://leetcode.com/problems/sort-an-array/
+ *
+ *
  * 1. 快速排序，各种写法
  *
  */
