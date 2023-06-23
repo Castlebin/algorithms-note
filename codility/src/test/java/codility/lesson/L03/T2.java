4f1f98a61073ab55ebb719f4cf9e03bc31f1554c
Castlebin <Castlebin@gmail.com>
2023-06-23 22:29:10 +0800

    PermMissingElem
     Find the missing element in a given permutation.
    
     https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/

---
+++ codility/src/test/java/codility/lesson/L03/T2.java
@@ -0,0 +1,50 @@
+package codility.lesson.L03;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ PermMissingElem
+
+ Find the missing element in a given permutation.
+
+ https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
+ */
+public class T2 {
+
+    /**
+     * 直接数学方法求解
+     * 时间复杂度 O(N)，空间复杂度 O(1)
+     */
+    public int solution(int[] A) {
+        int n = A.length + 1;
+        long result = (long) n * (n + 1) / 2;
+        for (int num : A) {
+            result -= num;
+        }
+        return (int) result;
+    }
+
+    /**
+     * 申请大小为 N + 1 的额外空间的数组，标记数字是否出现
+     * 时间复杂度 O(N)，空间复杂度 O(N)
+     */
+    public int solution_2(int[] A) {
+        boolean[] flag = new boolean[A.length + 1];
+        for (int num : A) {
+            flag[num - 1] = true;
+        }
+        for (int i = 0; i < flag.length; i++) {
+            if (!flag[i]) {
+                return i + 1;
+            }
+        }
+        return A.length + 1;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(4, solution(new int[] {2, 3, 1, 5}));
+    }
+
+}
