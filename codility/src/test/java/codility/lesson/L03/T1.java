b3fae839887e754fd3cb48fecc514c50afadb656
Castlebin <Castlebin@gmail.com>
2023-06-23 22:14:14 +0800

    FrogJmp
     Count minimal number of jumps from position X to Y.
    
     https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/

---
+++ codility/src/test/java/codility/lesson/L03/T1.java
@@ -0,0 +1,29 @@
+package codility.lesson.L03;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ FrogJmp
+
+ Count minimal number of jumps from position X to Y.
+
+ https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+ */
+public class T1 {
+
+    public int solution(int X, int Y, int D) {
+        int sub = Y - X;
+        int step = sub / D;
+        if (sub % D > 0) {
+            step++;
+        }
+        return step;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(3, solution(10, 85, 30));
+    }
+
+}
