e8e04d81b77a6f721ab142548314f291e83598e1
Castlebin <Castlebin@gmail.com>
2023-06-24 18:15:59 +0800

    Flags
     Find the maximum number of flags that can be set on mountain peaks.
    
     https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/flags/

---
+++ codility/src/test/java/codility/lesson/L10/T3.java
@@ -0,0 +1,27 @@
+package codility.lesson.L10;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ Flags
+
+ Find the maximum number of flags that can be set on mountain peaks.
+
+ https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/flags/
+ */
+public class T3 {
+
+    /**
+     * TODO 解题
+     */
+    public int solution(int[] A) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(3, solution(new int[] {1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+    }
+
+}
