5117479b7855107a48a8aabd2214cb443431d38a
Castlebin <Castlebin@gmail.com>
2023-06-27 00:01:59 +0800

    206. 反转链表
     两种解法：
     1. 迭代的普通解法
     2. 递归解法（有趣）

--- leetcode/src/test/java/leetcode/N200_N299/T206.java
+++ leetcode/src/test/java/leetcode/N200_N299/T206.java
@@ -15,30 +15,34 @@ import leetcode.base.ListNode;
  */
 public class T206 {
 
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode cur = head;
-        while (cur != null) {
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
+    class Solution_1 {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode cur = head;
+            while (cur != null) {
+                ListNode next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            }
+            return prev;
         }
-        return prev;
     }
 
     /**
      * 递归解法。写 K 个一组反转链表，递归也很好用。
      */
-    public ListNode reverseList2(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    class Solution_2 {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode next = head.next;
+            ListNode newHead = reverseList(next);
+            head.next = null;
+            next.next = head;
+            return newHead;
         }
-        ListNode next = head.next;
-        ListNode newHead = reverseList2(next);
-        head.next = null;
-        next.next = head;
-        return newHead;
     }
 
     @Test
