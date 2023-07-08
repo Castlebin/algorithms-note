116d6a829288fcce85c57ac336014521241a1a6c
Castlebin <Castlebin@gmail.com>
2023-07-08 19:09:50 +0800

    代码模版

--- codility/src/test/java/codility/T00.java
+++ codility/src/test/java/codility/T00.java
@@ -4,20 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
-
+ 代码模版
  */
 public class T00 {
 
     /**
      * TODO 解题
      */
-    public int solution(int[] A) {
-        return 0;
+    class Solution_1 {
+        public int solution(int[] A) {
+            return 0;
+        }
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(3, solution(new int[] {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+        Assert.assertEquals(0, new Solution_1().solution(new int[] {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
     }
 
 }
