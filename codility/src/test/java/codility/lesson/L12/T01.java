d134fcc2af7b98a78d9067712089de7931d12917
Castlebin <Castlebin@gmail.com>
2023-07-09 14:20:59 +0800

    12-Euclidean algorithm

---
+++ codility/src/test/java/codility/lesson/L12/T01.java
@@ -0,0 +1,25 @@
+package codility.lesson.L12;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ 代码模版
+ */
+public class T01 {
+
+    /**
+     * TODO 解题
+     */
+    class Solution_1 {
+        public int solution(int[] A) {
+            return 0;
+        }
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(0, new Solution_1().solution(new int[] {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+    }
+
+}
