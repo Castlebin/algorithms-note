0efa923b12fb68e3b5e9de14a40da4ba26a97f5e
Castlebin <Castlebin@gmail.com>
2023-12-12 00:10:27 +0800

    MaxCounters
     Calculate the values of counters after applying all alternating operations:
     increase counter by 1; set value of all counters to current maximum.

--- codility/src/test/java/codility/lesson/L04/T3.java
+++ codility/src/test/java/codility/lesson/L04/T3.java
@@ -22,17 +22,22 @@ public class T3 {
         int[] result = new int[N];
         int max = 0;
         int lastMax = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] >= 1 && A[i] <= N) {
+        for (int a : A) {
+            if (a >= 1 && a <= N) {
+                int index = a - 1; // 计算下标
                 // 巧妙的在下一次才更新，而不是每次 max 操作的时候，都要更新所有的值
-                result[A[i] - 1] = Math.max(result[A[i] - 1], lastMax);
-                result[A[i] - 1]++;
-                max = Math.max(max, result[A[i] - 1]);
-            } else if (A[i] == N + 1) {
+                result[index] = Math.max(result[index], lastMax);
+
+                result[index]++; // 正常的 +1 操作
+
+                max = Math.max(max, result[index]);
+            } else if (a == N + 1) {
+                // 只是记下来最大值，下一次才更新
                 lastMax = max;
             }
         }
-        // 因为上面是延迟更新（下一次才更新），所以，需要检查，哪些值还没有执行 max 操作
+
+        // 最后一次的 check， 因为上面是延迟更新（下一次才更新），所以，需要检查，哪些值还没有执行 max 操作
         for (int i = 0; i < N; i++) {
             result[i] = Math.max(result[i], lastMax);
         }
@@ -40,15 +45,16 @@ public class T3 {
     }
 
     /**
-     * 时间复杂度为 O(N * M)
+     * 时间复杂度为 O(N * M)。   性能不行，会超时
      */
     public int[] solution_0(int N, int[] A) {
         int[] result = new int[N];
         int max = 0;
         for (int a : A) {
             if (a >= 1 && a <= N) {
-                result[a - 1]++;
-                max = Math.max(max, result[a - 1]);
+                int index = a - 1;
+                result[index]++;
+                max = Math.max(max, result[index]);
             } else if (a == N + 1) {
                 // 这个操作导致时间复杂度高！！
                 Arrays.fill(result, max);
