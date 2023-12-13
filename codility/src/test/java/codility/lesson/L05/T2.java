86aae1ae2ed75c7ce17db5b19322961541a7172c
Castlebin <Castlebin@gmail.com>
2023-12-14 01:00:36 +0800

    CountDiv  Compute number of integers divisible by k in range [a..b].

--- codility/src/test/java/codility/lesson/L05/T2.java
+++ codility/src/test/java/codility/lesson/L05/T2.java
@@ -9,15 +9,16 @@ import org.junit.Test;
 
  https://app.codility.com/programmers/lessons/5-prefix_sums/count_div/
 
- 这道题似乎和 前缀和 关系不大。
- 直接用数学方法求解的，时间复杂度 O(1)
+ 可以看做是一种前缀和思想的使用。 X / K  代表的是 [0, X] 中 K 的倍数的个数。
+ 所以 [A, B] 中 K 的倍数的个数就是 [0, B] 中 K 的倍数的个数减去 [0, A - 1] 中 K 的倍数的个数。
+ 再考虑到 A 可能是 K 的倍数，此时还要加 1。
  */
 public class T2 {
 
     public int solution(int A, int B, int K) {
         int result = (B / K) - (A / K);
         if (A % K == 0) {
-            result++;
+            result += 1;
         }
         return result;
     }
