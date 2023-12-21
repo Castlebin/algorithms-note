008607101375b6669d07fc073910bdaab687512e
Castlebin <Castlebin@gmail.com>
2023-12-21 19:52:39 +0800

    Ladder
     Count the number of different ways of climbing to the top of a ladder.

--- codility/src/test/java/codility/lesson/L13/T02.java
+++ codility/src/test/java/codility/lesson/L13/T02.java
@@ -4,22 +4,34 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- 代码模版
+ Ladder
+
+ Count the number of different ways of climbing to the top of a ladder.
  */
 public class T02 {
 
-    /**
-     * TODO 解题
-     */
-    class Solution_1 {
-        public int solution(int[] A) {
-            return 0;
+    public int[] solution(int[] A, int[] B) {
+        // 首先，求出 A 中最大的数。这个决定了最大的斐波那契数列的长度
+        int max = 0;
+        for (int index = 0; index < A.length; index++) {
+            max = Math.max(max, A[index]);
         }
+        int[] fibs = new int[max + 2];
+        fibs[0] = 0;
+        fibs[1] = 1;
+        for (int index = 2; index < fibs.length; index++) {
+            fibs[index] = (fibs[index - 1] + fibs[index - 2]) % (1 << 30); // 注意，因为结果要对 2^B[i] 取模，所以这里对 2^30 取模，用位运算。否则答案是不准确的
+        }
+        int[] result = new int[A.length];
+        for (int index = 0; index < A.length; index++) {
+            result[index] = fibs[A[index] + 1] % (1 << B[index]);
+        }
+        return result;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(0, new Solution_1().solution(new int[] {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+        Assert.assertArrayEquals(new int[]{5, 1, 8, 0, 1}, solution(new int[]{4, 4, 5, 5, 1}, new int[]{3, 2, 4, 3, 1}));
     }
 
 }
