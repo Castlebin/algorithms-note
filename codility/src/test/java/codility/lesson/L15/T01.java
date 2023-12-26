996a1368697690dbc0d7a35ed8244e5142d737ca
Castlebin <Castlebin@gmail.com>
2023-12-27 00:52:52 +0800

    Lesson 15 Caterpillar method

---
+++ codility/src/test/java/codility/lesson/L15/T01.java
@@ -0,0 +1,26 @@
+package codility.lesson.L15;
+
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ AbsDistinct
+
+ Compute number of distinct absolute values of sorted array elements.
+ */
+public class T01 {
+
+    public int solution(int[] A) {
+        return -1;
+    }
+
+    
+
+    @Test
+    public void test() {
+        Assert.assertEquals(5, solution(new int[] {-5, -3, -1, 0, 3, 6}));
+    }
+
+}
