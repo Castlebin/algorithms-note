3d8c88b07b4ed3ec97efc56ee22ef1de7cb5f12a
Castlebin <Castlebin@gmail.com>
2023-12-27 00:15:02 +0800

    Lesson 14 Binary search algorithm

---
+++ codility/src/test/java/codility/lesson/L14/T01.java
@@ -0,0 +1,22 @@
+package codility.lesson.L14;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ MinMaxDivision
+
+ Divide array A into K blocks and minimize the largest sum of any block.
+ */
+public class T01 {
+
+    public int solution(int K, int M, int[] A) {
+        return -1;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(6, solution(3, 5, new int[] {2, 1, 5, 1, 2, 2, 2}));
+    }
+
+}
---
+++ codility/src/test/java/codility/lesson/L14/T02.java
@@ -0,0 +1,27 @@
+package codility.lesson.L14;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ NailingPlanks
+
+ Count the minimum number of nails that allow a series of planks to be nailed.
+ */
+public class T02 {
+
+    public int solution(int[] A, int[] B, int[] C) {
+        return -1;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(4, solution(new int[] {1, 4, 5, 8}, new int[] {4, 5, 9, 10}, new int[] {4, 6, 7, 10, 2}));
+    }
+
+}
