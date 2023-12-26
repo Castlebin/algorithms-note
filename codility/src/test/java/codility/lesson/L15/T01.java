ff4e335b4f5f9858797a5df84cef85a014ea7c81
Castlebin <Castlebin@gmail.com>
2023-12-27 00:53:08 +0800

    AbsDistinct
     Compute number of distinct absolute values of sorted array elements.

--- codility/src/test/java/codility/lesson/L15/T01.java
+++ codility/src/test/java/codility/lesson/L15/T01.java
@@ -16,7 +16,17 @@ public class T01 {
         return -1;
     }
 
-    
+    /**
+     非常简单直观的算法，借助了额外空间，时间复杂度 O(N*log(N)) 或者可视为 O(N)。空间复杂度 O(N)
+     本方法和课程所讲的毛毛虫法无关
+     */
+    public int solution_1(int[] A) {
+        Set<Integer> absSet = new HashSet<>();
+        for (int index = 0; index < A.length; index++) {
+            absSet.add(Math.abs(A[index]));
+        }
+        return absSet.size();
+    }
 
     @Test
     public void test() {
