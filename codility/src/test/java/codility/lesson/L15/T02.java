3864bc44913fe67e61e7d1ed4ac10b24cdb0bab3
Castlebin <Castlebin@gmail.com>
2023-12-27 01:30:45 +0800

    CountDistinctSlices
     Count the number of distinct slices (containing only unique numbers).

--- codility/src/test/java/codility/lesson/L15/T02.java
+++ codility/src/test/java/codility/lesson/L15/T02.java
@@ -4,17 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
+ CountDistinctSlices
 
+ Count the number of distinct slices (containing only unique numbers).
  */
 public class T02 {
 
-    public int solution(int[] A, int[] B, int[] C) {
+    public int solution(int M, int[] A) {
+        int MAX = 1_000_000_000;
+
+        int N = A.length;
+
+
         return -1;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(4, solution(new int[] {1, 4, 5, 8}, new int[] {4, 5, 9, 10}, new int[] {4, 6, 7, 10, 2}));
+        Assert.assertEquals(9, solution(6, new int[] {3, 4, 5, 5, 2}));
     }
 
 }
