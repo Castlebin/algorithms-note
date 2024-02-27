219b9fdc323f8a93203ad5c1aca0a227168da784
Castlebin <Castlebin@gmail.com>
2024-02-27 22:08:40 +0800

    17. Dynamic programming

--- codility/src/test/java/codility/lesson/L17/T01.java
+++ codility/src/test/java/codility/lesson/L17/T01.java
@@ -15,17 +15,15 @@ public class T01 {
     public int solution(int[] A) {
         int N = A.length;
         int[] dp = new int[N];
-        Arrays.fill(dp, Integer.MIN_VALUE);
+        Arrays.fill(dp, Integer.MIN_VALUE); // 先都初始化为一个最小值，一定不会是
         dp[0] = A[0];
 
         for (int index = 0; index < N; index++) {
             for (int num = 1; num <= 6 && index + num < N; num++) {
+                // 下一个到达的位置
                 int nextIndex = index + num;
-                if (dp[nextIndex] == Integer.MIN_VALUE) {// 还没有到达过
-                    dp[nextIndex] = dp[index] + A[nextIndex];
-                } else { // 曾经到达过
-                    dp[nextIndex] = Math.max(dp[nextIndex], dp[index] + A[nextIndex]);
-                }
+                // 下一个到达的位置的满足题意的最大值
+                dp[nextIndex] = Math.max(dp[nextIndex], dp[index] + A[nextIndex]);
             }
         }
 
