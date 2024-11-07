f1b63208e09cf656b20bd1f1aab709d6de1b0ae6
Castlebin <Castlebin@gmail.com>
2024-11-07 22:29:54 +0800

    145. 二叉树的后序遍历

---
+++ leetcode/src/test/java/leetcode/N100_N199/T145.java
@@ -0,0 +1,41 @@
+package leetcode.N100_N199;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import leetcode.base.TreeNode;
+
+/**
+ * 各种 树的遍历：
+ * 144. 二叉树的前序遍历 https://leetcode.cn/problems/binary-tree-preorder-traversal/
+ * 94. 二叉树的中序遍历 https://leetcode.cn/problems/binary-tree-inorder-traversal/
+ * 145. 二叉树的后序遍历 https://leetcode.cn/problems/binary-tree-postorder-traversal/
+ * 102. 二叉树的层序遍历 https://leetcode.cn/problems/binary-tree-level-order-traversal/
+ *
+ * 589. N 叉树的前序遍历 https://leetcode.cn/problems/n-ary-tree-preorder-traversal/
+ * 590. N 叉树的后序遍历 https://leetcode.cn/problems/n-ary-tree-postorder-traversal/
+ * 429. N 叉树的层序遍历 https://leetcode.cn/problems/n-ary-tree-level-order-traversal/
+ */
+
+/**
+ * 145. 二叉树的后序遍历
+ */
+public class T145 {
+
+    // 解法 1：递归
+    public List<Integer> postorderTraversal_1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traversal(root, res);
+        return res;
+    }
+
+    private void traversal(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        traversal(root.left, res);
+        traversal(root.right, res);
+        res.add(root.val);
+    }
+
+}
