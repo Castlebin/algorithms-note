a2852d6697703309f3ffde0d23ce6bc2507bfacc
Castlebin <Castlebin@gmail.com>
2024-11-14 10:40:30 +0800

    快速排序

--- leetcode/src/test/java/sort/QuickSort_1.java
+++ leetcode/src/test/java/sort/QuickSort_1.java
@@ -15,29 +15,30 @@ public class QuickSort_1 {
         if (nums == null || nums.length <= 1) {
             return;
         }
-        // 快速扫描一遍是否已经有序(一个优化而已)
-        boolean sorted = checkOrder(nums);
-        if (sorted) {
-            return;
-        }
         quickSort(nums, 0, nums.length - 1);
     }
 
     public void quickSort(int[] nums, int begin, int end) {
-        if (begin < end) {
-            int pivot = partition(nums, begin, end);
-            quickSort(nums, begin, pivot - 1);
-            quickSort(nums, pivot + 1, end);
+        if (begin >= end) {
+            return;
         }
+        int pivot = partition(nums, begin, end);
+        quickSort(nums, begin, pivot - 1);
+        quickSort(nums, pivot + 1, end);
     }
 
     /**
      * 最好记的快速排序写法，一般来说，记下这一种就行了
+     * partition 做了 4 件事情：
+     * 1. 选取主元
+     * 2. 将小于等于主元的元素放在左边，大于等于主元的元素放在右边
+     * 3. 将主元放在正确的位置上
+     * 4. 返回主元的位置
      */
     private int partition(int[] nums, int begin, int end) {
         // 简单的选取 begin 位置元素作为主元
-        int pivot = nums[begin];
-        // 注意！！这里直接用的 begin
+        int pivotIndex = begin;
+        int pivot = nums[pivotIndex];
         int left = begin;
         int right = end;
         while (left < right) {
@@ -62,15 +63,6 @@ public class QuickSort_1 {
         int tmp = nums[p1];
         nums[p1] = nums[p2];
         nums[p2] = tmp;
-    }
-
-    public boolean checkOrder(int[] nums) {
-        for (int index = 0; index < nums.length - 1; index++) {
-            if (nums[index] > nums[index + 1]) {
-                return false;
-            }
-        }
-        return true;
     }
 
     @Test
