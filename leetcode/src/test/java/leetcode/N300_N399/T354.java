90d74050f55890aabf5cc398ae9a52a16b234d76
Castlebin <Castlebin@gmail.com>
2025-02-09 22:39:11 +0800

    354. 俄罗斯套娃信封问题

--- leetcode/src/test/java/leetcode/N300_N399/T354.java
+++ leetcode/src/test/java/leetcode/N300_N399/T354.java
@@ -1,6 +1,8 @@
 package leetcode.N300_N399;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,29 +31,40 @@ public class T354 {
 
         // 对高度进行求最长递增子序列的最大值
         int N = envelopes.length;
-        int[] top = new int[N];
-        int piles = 0;
+        List<Integer> top = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             int height = envelopes[i][1];
-            int pos = piles;
-            int left = 0, right = piles;
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (top[mid] < height) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                    pos = mid;
-                }
+            // 使用二分查找在 top 列表中寻找插入 height 值的位置。即查找第一个大于等于 height 的数字的位置 (左侧)
+            int pos = findFirstGreatOrEqualThan(top, height);
+            if (pos == -1) { // 没找到，说明应该新起一堆
+                top.add(height);
+            } else {
+                top.set(pos, height); // 更新这个位置的值为 height，表示将这个值放入了这一堆牌的堆顶了
             }
-            if (pos == piles) {
-                piles++;
-            }
-            top[pos] = height;
         }
-
-        return piles;
+        return top.size();
     }
+
+    // nums是一个递增的排序数组，在 nums 中查找第一个 大于等于 target 的位置。没有的话返回 -1 （也可以选择返回 nums.size() ，在使用返回值的时候，自己注意判断好就行）
+    public int findFirstGreatOrEqualThan(List<Integer> nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums.get(mid) >= target) { // 当前 mid 是一个满足条件的位置
+                if (mid == 0 || nums.get(mid - 1) < target) {
+                    return mid;
+                } else {
+                    // 还可以往左边移动
+                    right = mid - 1;
+                }
+            } else if (nums.get(mid) < target) {
+                // 说明要再往右边去找，所以，将 left 往右移
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     /**
      * 排序，然后转化为求最长递增子序列长度的问题
      * 传统的求最长递增子序列的解法在这里会超时 （ 时间复杂度 O(N^2) ）
