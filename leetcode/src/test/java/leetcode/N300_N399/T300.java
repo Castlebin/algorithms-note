51d796dca5375d1872bbe887cfa5b304bbed4ba3
Castlebin <Castlebin@gmail.com>
2025-02-09 22:48:57 +0800

    300. 最长递增子序列

--- leetcode/src/test/java/leetcode/N300_N399/T300.java
+++ leetcode/src/test/java/leetcode/N300_N399/T300.java
@@ -1,6 +1,8 @@
 package leetcode.N300_N399;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +11,42 @@ import org.junit.Test;
  * 300. 最长递增子序列
  */
 public class T300 {
+
+    /**
+     * 使用二分查找解法，算法复杂度: O(NlogN)
+     **/
+    public int lengthOfLIS_3(int[] nums) {
+        List<Integer> top = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int pos = findFirstGreatOrEqualThan(top, nums[i]);
+            if (pos == -1) { // 没找到，说明要新起一堆
+                top.add(nums[i]);
+            } else { // 找到了，将它放入这堆的堆顶
+                top.set(pos, nums[i]);
+            }
+        }
+        return top.size();
+    }
+    // nums是一个递增的排序数组，在 nums 中查找第一个 大于等于 target 的位置。没有的话返回 -1 （也可以选择返回 nums.size() ，在使用返回值的时候，自己注意判断好就行）
+    public int findFirstGreatOrEqualThan(List<Integer> nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums.get(mid) >= target) { // 当前 mid 是一个满足条件的位置
+                if (mid == 0 || nums.get(mid - 1) < target) {
+                    return mid;
+                } else {
+                    // 还可以往左边移动
+                    right = mid - 1;
+                }
+            } else if (nums.get(mid) < target) {
+                // 说明要再往右边去找，所以，将 left 往右移
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
 
     /**
      * 最典型的 动态规划 解法，算法复杂度：O(N^2)
@@ -62,6 +100,10 @@ public class T300 {
         Assert.assertEquals(4, lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
         Assert.assertEquals(4, lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
         Assert.assertEquals(1, lengthOfLIS(new int[]{7, 7, 7, 7, 7, 7, 7}));
+
+        Assert.assertEquals(4, lengthOfLIS_3(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+        Assert.assertEquals(4, lengthOfLIS_3(new int[]{0, 1, 0, 3, 2, 3}));
+        Assert.assertEquals(1, lengthOfLIS_3(new int[]{7, 7, 7, 7, 7, 7, 7}));
     }
     
 }
